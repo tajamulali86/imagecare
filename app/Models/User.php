602d4ad8e0b3d032311f3ccff0972a2e63c813e1
tajamul86 <tajamulali86@gmail.com>
2023-11-05 01:55:36 +0530

    a

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
+
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -21,7 +24,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role==="admin";
+    }
 
     /**
      * The attributes that should be hidden for serialization.
